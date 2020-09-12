1bbee7a2defa57bfea50547090d32c80de674697
yijie4188 <yijie4188@gmail.com>
2020-09-12 18:33:47 +0800

    添加错误控制

---
+++ src/main/java/com/hniecs/mainserver/controller/ErrorControler.java
@@ -0,0 +1,18 @@
+package com.hniecs.mainserver.controller;
+
+import com.hniecs.mainserver.tool.api.CommonResult;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author  yijie
+ * @desc    错误控制 ErrorControler.js
+ * @date    2020-09-12 18:26
+ * @logs[0] 2020-09-12 18:26 yijie 创建了ErrorControler.js文件
+ */
+@RestController
+public class ErrorControler {
+    @PostMapping(value = "/error/notFound")
+    public CommonResult notFound() {
+        return CommonResult.notFound();
+    }
+}
--- src/main/java/com/hniecs/mainserver/controller/LoginController.java
+++ src/main/java/com/hniecs/mainserver/controller/LoginController.java
@@ -2,14 +2,11 @@ package com.hniecs.mainserver.controller;
 
 import com.hniecs.mainserver.entity.User;
 import com.hniecs.mainserver.tool.api.CommonResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
-    @RequestMapping(value = "/user/base/login", method = RequestMethod.POST)
+    @PostMapping(value = "/user/base/login")
     public CommonResult login(@RequestBody User user) {
         if (user.getUserName().equals("admin") && user.vertifyPWD("123")) {
             return CommonResult.success("admin");
