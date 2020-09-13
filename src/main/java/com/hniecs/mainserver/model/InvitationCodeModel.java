bbab9d25b63e9bf9336dfa27a02ea2f643d4e2f0
yijie4188 <yijie4188@gmail.com>
2020-09-13 20:29:31 +0800

    预留inviteationCodeModel层待实现

--- src/main/java/com/hniecs/mainserver/model/InvitationCodeModel.java
+++ src/main/java/com/hniecs/mainserver/model/InvitationCodeModel.java
@@ -1,7 +1,15 @@
 package com.hniecs.mainserver.model;
 
+import com.hniecs.mainserver.dao.InvitationCodeDao;
+import com.hniecs.mainserver.entity.InvitationCodeEntity;
+import com.hniecs.mainserver.entity.UserEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * @desc    InvitationCodeModel.java
@@ -12,4 +20,31 @@ import org.springframework.stereotype.Repository;
 @Slf4j
 @Repository
 public class InvitationCodeModel {
+    @Resource
+    @Autowired
+    private InvitationCodeDao invitationCodeDao;
+
+    /**
+     * TODO lh 添加验证码组
+     * @param user              用户实体
+     * @param canInviteCount    能邀请的用户个数
+     * @param invitationCodes   验证码数组
+     */
+    public String addInvitationCodes(
+        UserEntity user,
+        int canInviteCount,
+        ArrayList<String> invitationCodes
+    ) {
+        return "";
+    }
+
+    /**
+     * TODO lh 验证码数组 使用一个验证码
+     * @param invitationCode   验证码实体
+     */
+    public String useInvitationCode(
+        InvitationCodeEntity invitationCode
+    ) {
+        return "";
+    }
 }
