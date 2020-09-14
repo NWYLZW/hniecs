65d06396ed833a42bf652dae3473162f807f808b
zerohua <zerohua99@163.com>
2020-09-14 12:53:40 +0800

    revert ci

--- src/main/java/com/hniecs/mainserver/dao/InvitationCodeDao.java
+++ src/main/java/com/hniecs/mainserver/dao/InvitationCodeDao.java
@@ -1,8 +1,6 @@
 package com.hniecs.mainserver.dao;
 
-import com.hniecs.mainserver.entity.InvitationCodeEntity;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * @desc    InvitationCodeDao.java
@@ -13,8 +11,6 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface InvitationCodeDao {
     // TODO czl 根据(创建用户id|邀请码id|邀请码状态) 查 邀请码
-    @Select("select * from ")
-    InvitationCodeEntity getInvitationCodeByUserId(Long uId);
 
     // TODO czl 增 邀请码
     // TODO czl 根据(创建用户id|邀请码id|邀请码状态) 删 邀请码
