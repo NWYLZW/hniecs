f08293da9d1bfdaa65a16763b840a757aed8a407
zerohua <zerohua99@163.com>
2020-09-14 22:11:44 +0800

    完善了findAbleUse

--- src/main/java/com/hniecs/mainserver/model/InvitationCodeModel.java
+++ src/main/java/com/hniecs/mainserver/model/InvitationCodeModel.java
@@ -24,51 +24,60 @@ public class InvitationCodeModel {
     private InvitationCodeDao invitationCodeDao;
 
     /**
-     * TODO lh 根据邀请码搜索数据库中能狗被使用的邀请码实体，没搜索到返回null
+     * 查询此邀请码是否能用
      * @param invitationCode    邀请码
+     * @logs[] 2020-09-14 21:56 zerohua
      */
     public InvitationCodeEntity findAbleUse(String invitationCode) {
-        return null;
+
+        InvitationCodeEntity entity = invitationCodeDao
+            .getOne(InvitationCodeDao.columnName.invitation_code,invitationCode);
+
+        if( entity != null && entity.getStatus() != 2 && entity.getAvailableInviteCount() > 0) {
+            return entity;
+        }else {
+            return null;
+        }
     }
 
     /**
-     * TODO lh 添加邀请码组
      * 一个用户对应一个邀请码，每个用户他的邀请码有效次数有限
-     * @param user                  用户实体
+     * @param user              用户实体
      * @param availableInviteCount  能邀请的用户个数
-     * @param invitationCodes       邀请码数组
+     * @param invitationCodes   邀请码数组
+     * @logs[] 2020-09-14 21:48 zerohua
      */
     public String addInvitationCodes(
         UserEntity user,
         int availableInviteCount,
         ArrayList<String> invitationCodes
     ) {
-        InvitationCodeEntity entity = new InvitationCodeEntity();
-        entity.setMtime(new Date());
-        entity.setCtime(new Date());
-        entity.setCreateUserId(user.getId());
-        entity.setStatus(0);
-        entity.setAvailableInviteCount(availableInviteCount);
-
         for (String invitationCode : invitationCodes) {
+            InvitationCodeEntity entity = new InvitationCodeEntity();
+            entity.setMtime(new Date());
+            entity.setCtime(new Date());
+            entity.setCreateUserId(user.getId());
             entity.setInvitationCode(invitationCode);
+            entity.setAvailableInviteCount(availableInviteCount);
+            entity.setStatus(0);
             try {
                 invitationCodeDao.insert(entity);
             } catch (Exception e) {
                 e.printStackTrace();
-                return "插入失败, 创建者:"+user.getUserName() + "执行时间:" + new Date();
+                return "插入失败,创建者:"+user.getUserName()+"执行时间:"+new Date();
             }
         }
         return "0";
     }
 
     /**
-     * TODO lh 邀请码数组 使用一个邀请码
      * @param invitationCode   邀请码实体
+     * @logs[] 2020-09-14 21:48 zerohua
      */
     public String useInvitationCode(InvitationCodeEntity invitationCode) {
 
-        invitationCodeDao.getAll(InvitationCodeDao.columnName.invitation_code,invitationCode.getInvitationCode());
+        InvitationCodeEntity entity = invitationCodeDao
+            .getOne(InvitationCodeDao.columnName.invitation_code,invitationCode.getInvitationCode());
         int count = invitationCode.getAvailableInviteCount();
         if(count > 0) {
             invitationCode.setAvailableInviteCount(count - 1);
