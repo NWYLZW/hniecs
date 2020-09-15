7599ba9f44daba4e8fd8267133b071801061f8e2
yijie4188 <yijie4188@gmail.com>
2020-09-15 20:16:38 +0800

    添加了登陆接口

--- src/main/java/com/hniecs/mainserver/controller/UserBaseController.java
+++ src/main/java/com/hniecs/mainserver/controller/UserBaseController.java
@@ -15,9 +15,10 @@ import java.util.*;
 /**
  * @author  yijie
  * @desc    UserBaseController.java
- * @date    2020-09-13
- * @logs[0] yijie 2020-09-13 创建了文件UserBaseController.java
- * @logs[1] yijie 2020-09-13 修改了文件名
+ * @date    2020-09-13 00:00
+ * @logs[0] 2020-09-13 00:00 yijie 创建了文件UserBaseController.java
+ * @logs[1] 2020-09-13 00:00 yijie 修改了文件名
+ * @logs[2] 2020-09-15 20:15 yijie 添加了登陆接口
  */
 @RestController
 @Slf4j
@@ -26,6 +27,13 @@ public class UserBaseController {
     @Resource
     private UserBaseService userBaseService;
 
+    /**
+     * 用户登陆
+     * @param user      用户信息
+     * @param session   httpSession，由mvc容器自己管理
+     * @bodyParam userName    Y   ""    用户名
+     * @bodyParam password    Y   ""    密码
+     */
     @PostMapping("/user/base/login")
     public CommonResult login(@RequestBody Map<String, String> user, HttpSession session) {
         String msg = userBaseService.login(
@@ -46,6 +54,27 @@ public class UserBaseController {
             return CommonResult.validateFailed(msg);
         }
     }
+
+    /**
+     * 用户登出
+     * @param session   httpSession，由mvc容器自己管理
+     */
+    @GetMapping("/user/base/logout")
+    public CommonResult logout(HttpSession session) {
+        Object sessionToken = session.getAttribute("sessionToken");
+        if (sessionToken != null) {
+            session.removeAttribute("sessionToken");
+        }
+        return CommonResult.success(null, "退出登陆成功");
+    }
+
+    /**
+     * 用户注册信息
+     * @param registerData  注册信息字典
+     * @bodyParam userName          Y   ""    用户名
+     * @bodyParam password          Y   ""    密码
+     * @bodyParam invitationCode    Y   ""    邀请码
+     */
     @PostMapping("/user/base/register")
     public CommonResult register(@RequestBody Map<String, String> registerData) {
         String msg = userBaseService.registerNewUser(
@@ -59,6 +88,4 @@ public class UserBaseController {
             return CommonResult.validateFailed(msg);
         }
     }
-
-    //退出登录
 }
