fb14c41d4b0dadd1e4cbb7eb128ff4d99c5a056e
yijie4188 <yijie4188@gmail.com>
2020-09-15 15:52:21 +0800

    完善一些内容

--- src/main/java/com/hniecs/mainserver/dao/InvitationCodeDao.java
+++ src/main/java/com/hniecs/mainserver/dao/InvitationCodeDao.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
  * @desc    InvitationCodeDao.java
  * @author  yijie
  * @date    2020-09-13 19:01
- * @logs[0] 2020-09-13 19:01 yijie 创建了InvitationCodeDao.java文件
- * @note    插入列名格式为 ${columnName}
+ * @logs[0] 2020-09-13 19:01 yijie      创建了InvitationCodeDao.java文件
+ * @logs[1] 2020-09-13 19:01 zerohua    插入列名格式为 ${columnName}
  */
 @Mapper
 public interface InvitationCodeDao {
@@ -33,6 +33,13 @@ public interface InvitationCodeDao {
      */
     @Select("select * from invitation_code where ${columnName}=#{condition}")
     public InvitationCodeEntity getOne(@Param("columnName") columnName col,int condition);
+    /**
+     *搜索返回单个数据
+     * @param col 列名
+     * @param condition 条件
+     */
+    @Select("select * from invitation_code where ${columnName}=#{condition}")
+    public InvitationCodeEntity getOne(@Param("columnName") columnName col,String condition);
 
     /**
      * 插入数据
--- src/main/java/com/hniecs/mainserver/model/InvitationCodeModel.java
+++ src/main/java/com/hniecs/mainserver/model/InvitationCodeModel.java
@@ -15,6 +15,7 @@ import java.util.Date;
  * @author  yijie
  * @date    2020-09-13 18:57
  * @logs[0] 2020-09-13 18:57 yijie 创建了InvitationCodeModel.java文件
+ * @logs[1] 2020-09-15 15:51 yijie 完善一些内容
  */
 @Repository
 @Slf4j
@@ -26,15 +27,16 @@ public class InvitationCodeModel {
     /**
      * 查询此邀请码是否能用
      * @param invitationCode    邀请码
-     * @logs[] 2020-09-14 21:56 zerohua
      */
     public InvitationCodeEntity findAbleUse(String invitationCode) {
+        InvitationCodeEntity ic = invitationCodeDao
+            .getOne(
+                InvitationCodeDao.columnName.invitation_code,
+                invitationCode
+            );
 
-        InvitationCodeEntity entity = invitationCodeDao
-            .getOne(InvitationCodeDao.columnName.invitation_code,invitationCode);
-
-        if( entity != null && entity.getStatus() != 2 && entity.getAvailableInviteCount() > 0) {
-            return entity;
+        if( ic != null && ic.getStatus() != 2 && ic.getAvailableInviteCount() > 0) {
+            return ic;
         }else {
             return null;
         }
@@ -42,48 +44,43 @@ public class InvitationCodeModel {
 
     /**
      * 一个用户对应一个邀请码，每个用户他的邀请码有效次数有限
-     * @param user              用户实体
+     * @param user                  用户实体
      * @param availableInviteCount  能邀请的用户个数
-     * @param invitationCodes   邀请码数组
-     * @logs[] 2020-09-14 21:48 zerohua
+     * @param invitationCodes       邀请码数组
      */
     public String addInvitationCodes(
         UserEntity user,
         int availableInviteCount,
         ArrayList<String> invitationCodes
     ) {
+        InvitationCodeEntity ic = new InvitationCodeEntity();
+        ic.setMtime(new Date());
+        ic.setCtime(new Date());
+        ic.setCreateUserId(user.getId());
+        ic.setAvailableInviteCount(availableInviteCount);
+        ic.setStatus(0);
         for (String invitationCode : invitationCodes) {
-            InvitationCodeEntity entity = new InvitationCodeEntity();
-            entity.setMtime(new Date());
-            entity.setCtime(new Date());
-            entity.setCreateUserId(user.getId());
-            entity.setInvitationCode(invitationCode);
-            entity.setAvailableInviteCount(availableInviteCount);
-            entity.setStatus(0);
+            ic.setInvitationCode(invitationCode);
             try {
-                invitationCodeDao.insert(entity);
+                invitationCodeDao.insert(ic);
             } catch (Exception e) {
                 e.printStackTrace();
-                return "插入失败,创建者:"+user.getUserName()+"执行时间:"+new Date();
+                return "插入失败, 创建者:" + user.getUserName() + "创建时间:" + new Date();
             }
         }
         return "0";
     }
 
     /**
-     * @param invitationCode   邀请码实体
-     * @logs[] 2020-09-14 21:48 zerohua
+     * @param invitationCode    邀请码实体
      */
     public String useInvitationCode(InvitationCodeEntity invitationCode) {
-
-        InvitationCodeEntity entity = invitationCodeDao
-            .getOne(InvitationCodeDao.columnName.invitation_code,invitationCode.getInvitationCode());
         int count = invitationCode.getAvailableInviteCount();
         if(count > 0) {
             invitationCode.setAvailableInviteCount(count - 1);
-            return "使用成功";
+            return "0";
         }else {
-            return "可用次数已用尽,使用失败!";
+            return "邀请码可用次数已用尽, 使用失败!";
         }
     }
 }
