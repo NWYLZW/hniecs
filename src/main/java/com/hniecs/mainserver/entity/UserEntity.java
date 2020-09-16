00c51f782ba3af3e16833f81b85e8f227fe9efcc
yijie4188 <yijie4188@gmail.com>
2020-09-16 22:38:19 +0800

    设置全部属性全部可获取

--- src/main/java/com/hniecs/mainserver/entity/UserEntity.java
+++ src/main/java/com/hniecs/mainserver/entity/UserEntity.java
@@ -1,6 +1,7 @@
 package com.hniecs.mainserver.entity;
 
 import com.hniecs.mainserver.tool.security.Password;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,32 +14,26 @@ import java.util.Date;
  * @logs[0] 2020-09-12 15:34 yijie 创建了该文件
  * @logs[1] 2020-09-13 01:47 yijie 添加了密码生成与校验
  * @logs[2] 2020-09-13 01:47 yijie 添加了创建时间与修改时间成员
+ * @logs[3] 2020-09-16 22:37 yijie 设置全部属性全部可获取
  */
+@Data
 public class UserEntity {
-
     /**
      * 用户id
      */
-    @Getter
     private long id;
-
     /**
      * 用户名
      */
-    @Getter
-    @Setter
     private String userName;
-
     /**
      * SHA算法加密后的密码
      */
     private String passwordSHA;
-
     /**
      * 创建时间
      */
     public Date ctime;
-
     /**
      * 修改时间
      */
