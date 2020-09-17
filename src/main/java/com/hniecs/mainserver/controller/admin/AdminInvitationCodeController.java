7de83c3a85c3cd180245e01e37ec8059eea2214a
yijie4188 <yijie4188@gmail.com>
2020-09-17 13:01:11 +0800

    设置任务 to 凌华

--- src/main/java/com/hniecs/mainserver/controller/admin/AdminInvitationCodeController.java
+++ src/main/java/com/hniecs/mainserver/controller/admin/AdminInvitationCodeController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class AdminInvitationCodeController {
-    // TODO 单个添加邀请码
-    // TODO 通过支付宝或微信导出账单文件 导入邀请码
-    // TODO 获取邀请码列表 根据多种筛选条件筛选 例如状态，内容，等等
-    // TODO 通过邀请码id 修改邀请码内容 禁用邀请码 添加标签 修改使用次数
+    // TODO lh 单个添加邀请码
+    // TODO lh 通过支付宝或微信导出账单文件 导入邀请码
+    // TODO lh 获取邀请码列表 根据多种筛选条件筛选 例如状态，内容，等等
+    // TODO lh 通过邀请码id 修改邀请码内容 禁用邀请码 添加标签 修改使用次数
 }
