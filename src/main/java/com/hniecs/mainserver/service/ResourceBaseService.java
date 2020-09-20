40f8920a26b16058345105832c3fbf111dd1ffdb
yijie4188 <yijie4188@gmail.com>
2020-09-20 20:39:33 +0800

    fix conflex

--- src/main/java/com/hniecs/mainserver/service/ResourceBaseService.java
+++ src/main/java/com/hniecs/mainserver/service/ResourceBaseService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Map;
 
 /**
@@ -23,16 +24,16 @@ public class ResourceBaseService {
      * 通过类型返回ResourceEntity数组
      * @param kind 资源种类
      */
-    public String getResourceByKind(String kind,ArrayList<ResourceEntity> resourceList){
-        return resourceModel.getByKind(kind,resourceList);
+    public String getResourceByKind(String kind, ArrayList<ResourceEntity> resourceList) {
+        return resourceModel.getByKind(kind, resourceList);
     }
 
     /**
      * 模糊搜索
      * @param condition 模糊搜索条件
      */
-    public String getByFuzzySearch(String condition, ArrayList<ResourceEntity> resourceList){
-        return resourceModel.getFuzzySearch(condition,resourceList);
+    public String getByFuzzySearch(String condition, ArrayList<ResourceEntity> resourceList) {
+        return resourceModel.getFuzzySearch(condition, resourceList);
 
     }
 
@@ -40,7 +41,7 @@ public class ResourceBaseService {
      * 根据id返回资源对象
      * @param id 资源id
      */
-    public ResourceEntity getResourceById(long id){
+    public ResourceEntity getResourceById(long id) {
         ResourceEntity resource = resourceModel.getById(id);
         return resource;
     }
@@ -49,34 +50,39 @@ public class ResourceBaseService {
      * 更新对象
      * @param resourceDate 资源对象
      */
-    public String updateResource(Map<String, String> resourceDate){
+    public String updateResource(Map<String, String> resourceDate) {
         long id = Long.parseLong(resourceDate.get("id"));
         String kind = resourceDate.get("kind");
         String introduce = resourceDate.get("introduce");
         String name = resourceDate.get("name");
         String url = resourceDate.get("url");
-        if(resourceModel.haveById(id)){
+        if (resourceModel.haveById(id)) {
             return "资源不存在";
         }
-        return resourceModel.updateResourceById(id,name,url,introduce,kind);
+        return resourceModel.updateResourceById(id, name, url, introduce, kind);
     }
 
     /**
      * 删除资源
      * @param id 资源id
      */
-    public String deleteResource(long id){
-        if(resourceModel.haveById(id)){
+    public String deleteResource(long id) {
+        if (resourceModel.haveById(id)) {
             return "资源不存在";
         }
         return resourceModel.deleteResource(id);
     }
 
     /**
-     *
+     * @param kind      资源种类
+     * @param introduce 资源介绍
+     * @param name      资源名
+     * @param url       资源链接
+     * @param table     获取返回值的table
      */
-    public String addResource(String kind, String introduce, String name, String url){
-        ResourceEntity resourceEntity=new ResourceEntity(name, url, introduce, kind);
+    public String addResource(String kind, String introduce, String name, String url, Hashtable table) {
+        ResourceEntity resourceEntity = new ResourceEntity(name, url, introduce, kind);
+        table.put("资源实体", resourceEntity);
         return resourceModel.addNew(resourceEntity);
     }
 }
