5733b5846345aaf4892024ce8fad26df12102021
zerohua <zerohua99@163.com>
2020-09-20 21:51:55 +0800

    update serive 完善了插入验证码功能,未做测试

--- src/main/java/com/hniecs/mainserver/service/admin/InvitationCodeService.java
+++ src/main/java/com/hniecs/mainserver/service/admin/InvitationCodeService.java
@@ -1,6 +1,5 @@
 package com.hniecs.mainserver.service.admin;
 
-import com.hniecs.mainserver.entity.InvitationCodeEntity;
 import com.hniecs.mainserver.entity.UserEntity;
 import com.hniecs.mainserver.model.InvitationCodeModel;
 import com.hniecs.mainserver.tool.excel.ExcelRader;
@@ -8,12 +7,12 @@ import com.hniecs.mainserver.tool.excel.bill.AlipayBillExcel;
 import com.hniecs.mainserver.tool.excel.bill.BillExcel;
 import com.hniecs.mainserver.tool.excel.bill.WechatBillExcel;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 /**
  * @desc    InvitationCodeService.java
@@ -22,50 +21,92 @@ import java.util.Hashtable;
  * @logs[0] 2020-09-20 12:52 yijie 创建了InvitationCodeService.java文件
  */
 @Slf4j
-@Service
 public class InvitationCodeService {
+
+    /**
+     * 金额有效阈值
+     */
     private final int INVITATIONCODE_THRESHOLD_MONEY = 30;
+
     @Resource
-    InvitationCodeModel invitationCodeModel;
+    private InvitationCodeModel invitationCodeModel;
 
     public String addInvitationCodes(
-        UserEntity creater, int availableCount, String tagName, ArrayList<String> invitationCodes,
-        Hashtable returnData
-    ) {
-        // TODO lh 校验 invitationcodes 去除重复盛据
-        // TODO lh 将添加成功的邀请将以数组的格式适回
-        returnData.put("successCount",0);
-        returnData.put("count",0);
-        returnData.put("successAddList",new ArrayList<InvitationCodeEntity>());
+        UserEntity creator, int availableCount,
+        List<String> invitationCodes,String tagName,
+        Hashtable returnData) {
+
         String msg = invitationCodeModel.addInvitationCodes(
-            creater,availableCount,
-            // TODO lh 将 tagName 存入数据库
-            invitationCodes
+            creator,availableCount,
+            invitationCodes,
+            tagName,
+            returnData
         );
         return msg;
     }
 
+    /**
+     * @param creator          创建者
+     * @param availableCount   可利用次数
+     * @param tagName          支付类型
+     * @param returnData
+     * @return
+     */
     public String addInvitationCodes(
-        UserEntity creater, int availableCount, String tagName, InputStream ExcelIS,
-        Hashtable returnData
-    ) {
-        ArrayList<String> invitationCodes = new ArrayList<>();
-        try {
-            ArrayList<BillExcel> billExcels =
-                (ArrayList<BillExcel>) new ExcelRader<BillExcel>()
-                    .getBillList(ExcelIS, () -> {
-                        if (tagName.equals("ali")) {
-                            return AlipayBillExcel.class;
-                        } else if (tagName.equals("wechat")) {
-                            return WechatBillExcel.class;
-                        }
-                        return null;
-                    });
-        } catch (NullPointerException | ClassNotFoundException e) {
-            log.error(String.valueOf(e));
-            return "服务器错误";
+        UserEntity creator, int availableCount, String tagName,
+        InputStream excelIn,
+        Hashtable returnData) {
+
+        List<String> invitationCodeStr = getInvitationCodeStrList(excelIn,tagName);
+        return this.addInvitationCodes(creator, availableCount,
+            invitationCodeStr, tagName, returnData);
+
+    }
+
+    private List<String> getInvitationCodeStrList(InputStream excelIn,String tagName) {
+        List<BillExcel> billExcels = getBillExcel(excelIn, tagName);
+        return checkAndTransToList(billExcels);
+    }
+
+    /**
+     * 除去集合中不满足条件的实体
+     * @param billExcels
+     * @return 返回验证码集合
+     */
+    private List<String> checkAndTransToList(List<BillExcel> billExcels) {
+        List<String> list = new ArrayList<>();
+        for (BillExcel billExcel : billExcels) {
+            //字符变成钱
+            String moneyStr = billExcel.getMoney();
+            Long money = Long.valueOf(moneyStr);
+            if (money == INVITATIONCODE_THRESHOLD_MONEY) {
+                list.add(billExcel.getTransactionNumber());
+            }
         }
-        // TODO lh 将金额等于 INVITATIONCODE_THRESHOLD_MONEY 存入 invitationCodes 数组中
-        return this.addInvitationCodes(creater, availableCount, tagName, invitationCodes, returnData);
+        return list;
+    }
+
+    /**
+     * @param in        表格流
+     * @param tagName   支付方式
+     * @return          账单集合
+     */
+    private List<BillExcel> getBillExcel(InputStream in,String tagName) {
+
+        List<BillExcel> billExcels = null;
+        ExcelRader<BillExcel> rader = new ExcelRader<>();
+        try {
+            billExcels = rader.getBillList(in,() -> {
+                if(tagName.equals("微信")) {
+                    return WechatBillExcel.class;
+                }else if(tagName.equals("支付宝")) {
+                    return AlipayBillExcel.class;
+                }
+                return null;
+            });
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return billExcels;
     }
 }
