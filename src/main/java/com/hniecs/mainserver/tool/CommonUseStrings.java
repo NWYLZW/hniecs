a26b8bb4cfb4f7b874a0a805baf58a857a8cd822
yijie4188 <yijie4188@gmail.com>
2020-09-25 02:52:23 +0800

    [feat] (tool) 添加常用字符串枚举

---
+++ src/main/java/com/hniecs/mainserver/tool/CommonUseStrings.java
@@ -0,0 +1,16 @@
+package com.hniecs.mainserver.tool;
+
+/**
+ * @desc    CommonUseStrings.java
+ * @author  yijie
+ * @date    2020-09-25 00:04
+ * @logs[0] 2020-09-25 00:04 yijie      创建了CommonUseStrings.java文件
+ */
+public enum CommonUseStrings {
+    SERVER_FAILED("服务器响应错误");
+
+    public final String S;
+    CommonUseStrings(String s) {
+        S = s;
+    }
+}
