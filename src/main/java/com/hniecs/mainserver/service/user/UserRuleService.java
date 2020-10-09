fe7223e14f8007d3b470b106958efb6bb51009a9
yijie4188 <yijie4188@gmail.com>
2020-10-09 14:06:39 +0800

    [feat] (service) 根据用户的角色获取app列表

--- src/main/java/com/hniecs/mainserver/service/user/UserRuleService.java
+++ src/main/java/com/hniecs/mainserver/service/user/UserRuleService.java
@@ -1,12 +1,10 @@
 package com.hniecs.mainserver.service.user;
 
 import com.hniecs.mainserver.entity.AppEntity;
+import com.hniecs.mainserver.tool.security.SessionTool;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * @desc    用户角色相关service UserRuleService.java
@@ -21,29 +19,31 @@ public class UserRuleService {
      * @param returnData 返回数据类型
      */
     public String getApps (List<AppEntity> returnData) {
-        returnData.addAll(
-            new ArrayList<>(Arrays.asList(
+        ArrayList<AppEntity> apps = new ArrayList<>();
+        System.out.println(Objects.requireNonNull(SessionTool.curUser()).getDetail().getRule().getId() == 2);
+        if (Objects.requireNonNull(SessionTool.curUser()).getDetail().getRule().getId() == 2) {
+            apps.add(new AppEntity(
+                "后台管理", "管理员后台管理页面", "&#xe76e;",
+                "/admin/manage/user", "[2]", 0
+            ));
+        }
+        apps.add(new AppEntity(
+            "我的消息", "用户消息中心", "&#xe6a2;",
+            "/message/my/index", "", 0, new ArrayList<>(Arrays.asList(
                 new AppEntity(
-                    "后台管理", "管理员后台管理页面", "&#xe76e;",
-                    "/admin/manage/user", "[1]", 0
-                ),
-                new AppEntity(
-                    "我的消息", "用户消息中心", "&#xe6a2;",
-                    "/message/my/index", "", 0, new ArrayList<>(Arrays.asList(
-                    new AppEntity(
-                        "公告", "网站公告墙", "&#xe6aa;",
-                        "/admin/announcement/index", "", 0
-                    ), new AppEntity(
-                        "聊天", "网站聊天室", "&#xe6a9;",
-                        "/admin/chat/index", "", 0
-                    ), new AppEntity(
-                        "看板", "网站公告墙", "&#xe6ab;",
-                        "/admin/board/index", "", 0
-                    )
-                ))
+                    "公告", "网站公告墙", "&#xe6aa;",
+                    "/admin/announcement/index", "", 0
+                ), new AppEntity(
+                    "聊天", "网站聊天室", "&#xe6a9;",
+                    "/admin/chat/index", "", 0
+                ), new AppEntity(
+                    "看板", "网站公告墙", "&#xe6ab;",
+                    "/admin/board/index", "", 0
                 )
             ))
-        );
+        ));
+
+        returnData.addAll(apps);
         return "0";
     }
 }
